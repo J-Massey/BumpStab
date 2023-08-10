5725a84721041382946dfa6d87627e2c2e7ff718
Jonah Iridis <masseyjmo@gmail.com>
2023-08-10 19:55:25 +0100

    Trying to save data with hdf5

---
+++ data/0/uvp/save_h5.py
@@ -0,0 +1,23 @@
+import h5py
+import numpy as np
+
+u = np.load("u.npy")
+v = np.load("v.npy")
+p = np.load("p.npy")
+
+combined_data = np.stack([u, v, p], axis=0)
+
+# Sample metadata
+xlims = [-0.25, 2]
+ylims = [-0.35, 0.35]
+
+with h5py.File('uvp.hdf5', 'w') as f:
+    # Create a group for the datasets
+    group = f.create_group("data_group")
+    
+    # Attach metadata as attributes to the group
+    group.attrs['xlims'] = xlims
+    group.attrs['ylims'] = ylims
+
+    # Create datasets within the group
+    dset_combined = group.create_dataset('combined_data', data=combined_data, compression="lzf")
