3789e55b5843db07123ac230ef8493e5f4f81bbc
Jonah Iridis <masseyjmo@gmail.com>
2023-08-11 12:15:13 +0100

    Why's it not restarting This would be very, very annoying

--- data/0.001/128/run-rough.py
+++ data/0.001/128/run-rough.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 from lotus import run
 from changef90 import new_f90
 from pathlib import Path
+import numpy as np
 
 def extract_k():
     with open("lotus.f90","r") as fileSource:
@@ -15,7 +18,7 @@ def run_bumps(lam):
 
 def run_cont(lam):
     new_f90(lam)
-    run(1024, f'./{1/lam:.0f}_save', './128/')
+    run(1024, f'{cwd}/{1/lam:.0f}-save', f'./{1/lam:.0f}/')
 
 if __name__ == "__main__":
     cwd = Path.cwd()
