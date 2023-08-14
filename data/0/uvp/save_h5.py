3f39e19dc275f04825259a426dce2e79eed9f33a
Jonah Iridis <masseyjmo@gmail.com>
2023-08-14 13:38:10 +0100

    Reverting to npy. hdf5 takes an hour to load! This is unworkable for now, I need some results

--- data/0/uvp/save_h5.py
+++ data/0/uvp/save_h5.py
@@ -11,17 +11,19 @@ p = np.load("p.npy")
 
 combined_data = np.stack([u, v, p], axis=0)
 
-# Sample metadata
-xlims = [-0.35, 2]
-ylims = [-0.35, 0.35]
+np.save("uvp.npy", combined_data)
 
-with h5py.File('uvp.hdf5', 'w') as f:
-    # Create a group for the datasets
-    group = f.create_group("data_group")
+# # Sample metadata
+# xlims = [-0.35, 2]
+# ylims = [-0.35, 0.35]
+
+# with h5py.File('uvp.hdf5', 'w') as f:
+#     # Create a group for the datasets
+#     group = f.create_group("data_group")
     
-    # Attach metadata as attributes to the group
-    group.attrs['xlims'] = xlims
-    group.attrs['ylims'] = ylims
+#     # Attach metadata as attributes to the group
+#     group.attrs['xlims'] = xlims
+#     group.attrs['ylims'] = ylims
 
-    # Create datasets within the group
-    dset_combined = group.create_dataset('uvp', data=combined_data, compression="lzf")
+#     # Create datasets within the group
+#     dset_combined = group.create_dataset('uvp', data=combined_data, compression="lzf")
--- data/watch_simdir.py
+++ data/watch_simdir.py
@@ -1,55 +1,59 @@
 import os
 import time
 from tkinter import Tcl
-
+import numpy as np
 from lotusvis.flow_field import ReadIn
 
 
-def fluid_snap(sim_dir, fn, count):
-    fsim = ReadIn(sim_dir, 'fluid', 2048, ext="vti")
-    fsim.u_low_memory_saver(fn, count, "./data")
-    fsim.v_low_memory_saver(fn, count, "./data")
-    fsim.p_low_memory_saver(fn, count, "./data")
+def fluid_snap(sim_dir: str):
+    os.system(f"mkdir -p {os.path.abspath(sim_dir + '/..')}/uvp")
+    fsim = ReadIn(sim_dir, 'fluAv', 4096, ext="vti")
+    fsnap = fsim.snaps(save=True, part=False, save_path=f"{os.path.abspath(sim_dir + '/..')}/uvp")
+    np.save(f"{os.path.abspath(sim_dir + '/..')}/uvp/flow.npy", fsnap)
+    return fsnap
 
-def body_snap(sim_dir, fn, count):
-    bsim = ReadIn(sim_dir, 'bodyF', 2048, ext="vti")
-    bsim.save_sdf_low_memory(fn, count, "./data")
-
-
-# Specify the directory to monitor
-directory_to_watch = "./lotus-data"
-print("Watching directory:", directory_to_watch)
-
-
-bcount=0
-fcount = 0
-delete_count = 0
-# while True:
-for root, _, files in os.walk(directory_to_watch):
-    # Process files
-    for file in files:
-        if root.endswith("datp"):
-            # Sort the files
-            dpdfs = [fp for fp in os.listdir(root)]
-            dpdfs = Tcl().call("lsort", "-dict", dpdfs)
-            for fn in dpdfs:
-                if fn.startswith("bodyF"):
-                    path = os.path.join(root, fn)
-                    body_snap(directory_to_watch, path, bcount)
-                    os.remove(path)
-                    bcount += 1
-                elif fn.startswith("fluid"):
-                    path = os.path.join(root, fn)
-                    fluid_snap(directory_to_watch, path, fcount)
-                    os.remove(path)
-                    fcount += 1
-for root, _, files in os.walk(directory_to_watch):
-    for file in files:
-        if (file.startswith("fluid") or file.startswith("bodyF")) and \
-        (not file.endswith(".pvtr") and not file.endswith(".vtr") and not file.endswith("vtr.pvd")):
-            file_path = os.path.join(root, file)
-            os.remove(file_path)
-            delete_count += 1
+def body_snap(sim_dir: str):
+    os.system(f"mkdir -p {os.path.abspath(sim_dir + '/..')}/uvp")
+    bsim = ReadIn(sim_dir, 'bodAv', 4096, ext="vti")
+    bsnap = bsim.snaps(save=True, part=False, save_path=f"{os.path.abspath(sim_dir + '/..')}/uvp")
+    np.save(f"{os.path.abspath(sim_dir + '/..')}/uvp/body.npy", bsnap)
+    return bsnap.shape
 
 
-print(f"Total files deleted: {delete_count}")
+if __name__ == "__main__":
+    sim_dir = f"{os.getcwd()}/data/0.001/128/128-save"
+    print(fluid_snap(sim_dir).shape)
+    
+
+# bcount=0
+# fcount = 0
+# delete_count = 0
+# # while True:
+# for root, _, files in os.walk(directory_to_watch):
+#     # Process files
+#     for file in files:
+#         if root.endswith("datp"):
+#             # Sort the files
+#             dpdfs = [fp for fp in os.listdir(root)]
+#             dpdfs = Tcl().call("lsort", "-dict", dpdfs)
+#             for fn in dpdfs:
+#                 if fn.startswith("bodyF"):
+#                     path = os.path.join(root, fn)
+#                     body_snap(directory_to_watch, path, bcount)
+#                     os.remove(path)
+#                     bcount += 1
+#                 elif fn.startswith("fluid"):
+#                     path = os.path.join(root, fn)
+#                     fluid_snap(directory_to_watch, path, fcount)
+#                     os.remove(path)
+#                     fcount += 1
+# for root, _, files in os.walk(directory_to_watch):
+#     for file in files:
+#         if (file.startswith("fluid") or file.startswith("bodyF")) and \
+#         (not file.endswith(".pvtr") and not file.endswith(".vtr") and not file.endswith("vtr.pvd")):
+#             file_path = os.path.join(root, file)
+#             os.remove(file_path)
+#             delete_count += 1
+
+
+# print(f"Total files deleted: {delete_count}")
