86d8ea738d86b44d07dffea819b61de6201a78c3
Jonah Iridis <masseyjmo@gmail.com>
2023-08-16 12:52:44 +0100

    Changed module name

--- resolvent/save_DMD.py
+++ resolvent/save_DMD.py
@@ -1,7 +1,8 @@
-from scipy.linalg import svd, cholesky
+from scipy.linalg import cholesky
 import numpy as np
 from plot_field import plot_field
 import os
+import time
 
 
 class SaveDMD:
@@ -14,16 +15,22 @@ class SaveDMD:
 
     def _load_modes(self, dom):
         self.flucs = np.load(f"{self.path}/{self.dom}_flucs.npy")
-        self.Ub, self.Sigmab, self.VTb, self.Uf, self.Sigmaf, self.VTf = np.load(
-            f"{self.path}/{dom}_svd.npz", allow_pickle=True
-        )
+        with np.load(f"{self.path}/{dom}_svd.npz") as data:
+            self.Ub = data["Ub"]
+            self.Sigmab = data["Sigmab"]
+            self.VTb = data["VTb"]
+            self.Uf = data["Uf"]
+            self.Sigmaf = data["Sigmaf"]
+            self.VTf = data["VTf"]
         self.nx, self.ny, self.nt = np.load(f"{self.path}/{self.dom}_nxyt.npy")
 
     def fbDMD(self, r=None):
         dt = 14 / self.nt
         if r is None:
             r = self.nt - 1
-        # Sigma_plot(Sigma)
+
+        print(f"\n----- Calculating fbDMD with r={r} -----")
+        t0 = time.time()
         U_r = self.Ub[:, :r]
         Sigmar = np.diag(self.Sigmab[:r])
         VT_r = self.VTb[:r, :]
@@ -38,6 +45,7 @@ class SaveDMD:
 
         # Find the linear operator
         A_tilde = 1 / 2 * (Atildef + np.linalg.inv(Atildeb))
+
         rho, W = np.linalg.eig(A_tilde)
 
         # Find the eigenfunction from spectral expansion
@@ -45,6 +53,8 @@ class SaveDMD:
 
         # Find the DMD modes
         V_r = np.dot(np.dot(self.fluc2, VT_r.T), np.dot(np.linalg.inv(S_r), W))
+
+        print(f"fbDMD calculated in {time.time() - t0:.2f} seconds.")
 
         return Lambda, V_r
     
@@ -54,7 +64,7 @@ class SaveDMD:
         np.save(f"{self.path}/{self.dom}_V_r.npy", V_r)
     
 def F_tilde(V_r):
-    # Find the hermatian adjoint of the eigenvecs
+    # Find the hermatian adjoint of the eigenvectors
     V_r_star_Q = V_r.conj().T
     V_r_star_Q_V_r = np.dot(V_r_star_Q, V_r)
     # Cholesky factorization
@@ -65,13 +75,11 @@ def F_tilde(V_r):
 # Sample usage
 if __name__ == "__main__":
     case = "0.001/128"
-    doms = ["body", "wake"]
+    doms = ["wake"]
     for dom in doms:
         resolvent = SaveDMD(f"{os.getcwd()}/data/{case}/data", dom)
         resolvent.save_fbDMD()
-        resolvent.flucs.shape
-        print(resolvent.nx, resolvent.ny, resolvent.nt)
         q = resolvent.flucs.reshape(3, resolvent.nx, resolvent.ny, resolvent.nt)
-        pxs = np.linspace(0, 1, resolvent.nx)
-        pys = np.linspace(-0.25, 0.25, resolvent.ny)
+        pxs = np.linspace(1, 2, resolvent.nx)
+        pys = np.linspace(-0.35, 0.35, resolvent.ny)
         plot_field(q[1, :, :, 0].T, pxs, pys, f"figures/test_{dom}.pdf", lim=[-0.5, 0.5])
