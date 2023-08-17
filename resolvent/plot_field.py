55f8f6fc738f4b4250df3f0124f42d60c0827024
Jonah Iridis <masseyjmo@gmail.com>
2023-08-17 18:06:15 +0100

    Fucking mess, rough seems to be working

--- resolvent/plot_field.py
+++ resolvent/plot_field.py
@@ -4,11 +4,14 @@ import os
 from tkinter import Tcl
 
 import imageio
+from PIL import Image, ImageSequence
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 import scienceplots
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from tqdm import tqdm
+
 plt.style.use(["science"])
 plt.rcParams["font.size"] = "10.5"
 
@@ -19,7 +22,6 @@ def plot_field(qi, pxs, pys, path, _cmap="seismic", lim=None):
         lim = [np.min(qi), np.max(qi)]
     else:
         pass
-
     fig, ax = plt.subplots(figsize=(5, 3))
     levels = np.linspace(lim[0], lim[1], 45)
     _cmap = sns.color_palette(_cmap, as_cmap=True)
@@ -40,12 +42,11 @@ def plot_field(qi, pxs, pys, path, _cmap="seismic", lim=None):
     cax = divider.append_axes("top", size="7%", pad=0.2)
     fig.add_axes(cax)
     cb = plt.colorbar(cs, cax=cax, orientation="horizontal", ticks=levels[::11])
-
-    clevels = np.mean(lim)-lim[0]*np.array([-3/4, -1/2, -1/4, 1/4, 1/2, 3/4])
-    
+    clevels = np.mean(lim) - lim[0] * np.array(
+        [-3 / 4, -1 / 2, -1 / 4, 1 / 4, 1 / 2, 3 / 4]
+    )
     # Find which clevel corresponds to the closest level
     # clevel = np.argmin(np.abs(levels[:, None]-clevels[None, :]), axis=0)
-
     co = ax.contour(
         pxs,
         pys,
@@ -53,16 +54,14 @@ def plot_field(qi, pxs, pys, path, _cmap="seismic", lim=None):
         levels=clevels,
         vmin=lim[0],
         vmax=lim[1],
-        colors='black',
+        colors="black",
         linewidths=0.25,
         # alpha=0.85,
     )
-    ax.clabel(co, inline=True, fontsize=6, fmt='%.2f')
-
+    ax.clabel(co, inline=True, fontsize=6, fmt="%.2f")
     ax.set_aspect(1)
     plt.savefig(path, dpi=700)
     plt.close()
-
 
 
 def fns(dirn):
@@ -71,24 +70,27 @@ def fns(dirn):
     return fns
 
 
-def gif_gen(path, nom, duration):
-    images = []
-    for filename in tqdm(fns(path)):
-        images.append(imageio.v3.imread(os.path.join(path, filename), plugin="pillow", mode="RGBA"))
+def gif_gen(path, nom, gif_length):
+    images = [
+        imageio.v3.imread(os.path.join(path, filename), plugin="pillow", mode="RGBA")
+        for filename in tqdm(fns(path))
+    ]
 
+    # Calculate duration in milliseconds for the entire GIF length
+    dur = (gif_length * 1000) / len(images)
+
+    print(f"Writing GIF... with fps = {1000*(1/dur):.2f}")
+
+    # Write the GIF
     imageio.v3.imwrite(
         nom,
         images,
-        plugin="pillow",
-        mode="RGBA",
-        duration=duration,
+        duration=dur,  # Use duration directly instead of save_kwargs
         loop=0,
-        # transparency=100,
         disposal=2,
     )
 
 
 # Sample usage
 if __name__ == "__main__":
-    gif_gen("figures/rough-unwarp", "figures/rough-unwarp.gif", 1/200)
-
+    gif_gen("figures/rough-unwarp", "figures/rough_unwarp.gif", 4)
